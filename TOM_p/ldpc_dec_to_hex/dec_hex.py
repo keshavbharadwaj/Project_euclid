5849a2b95c23194ea00b91edefae07555e7cc57c
vivekadi <vivek.adishesha@gmail.com>
2020-11-30 18:10:25 +0530

    updates to ldpc hex and binary

--- TOM_p/ldpc_dec_to_hex/dec_hex.py
+++ TOM_p/ldpc_dec_to_hex/dec_hex.py
@@ -1,27 +1,38 @@
-import math
-
-
 def ram_file(k):
     f = open("C:/Users/vivek/Desktop/ldpc14_sram" + k + ".txt", "r")
-    dec = []
+    hexa = []
+    bina = []
+
     for x in f:
         for line in x.split("\n"):
             for val in line.split(" "):
                 if val == '':
                     continue
-                dec.append(hex(int(val)))
+                hexa.append(hex(int(val)))
+                bina.append(bin(int(val)))
 
-    print(dec)
+    print(hexa)
+    print(bina)
     f.close()
 
     i = 0
     f = open("C:/Users/vivek/Desktop/ldpc14_sram" + k + "_hex.txt", "w")
-    for hex_str in dec:
+    for hex_str in hexa:
         if i == int(k):
             f.write("\n")
             i = 0
         i += 1
         f.write(str(hex_str + ','))
+    f.close()
+
+    j = 0
+    f = open("C:/Users/vivek/Desktop/ldpc14_sram" + k + "_bin.txt", "w")
+    for bin_str in bina:
+        if j == int(k):
+            f.write("\n")
+            j = 0
+        j += 1
+        f.write(str(bin_str + ','))
     f.close()
 
 
